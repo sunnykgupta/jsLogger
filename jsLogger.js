5b3dabc47e129089d5274e5087c31e9e9fa3791e
Sunny R Gupta <sunnykgupta@users.noreply.github.com>
2018-05-21 14:16:00 +0530

    Fixed Issue #3
    https://github.com/sunnykgupta/jsLogger/issues/3

--- jsLogger.js
+++ jsLogger.js
@@ -14,19 +14,19 @@ window.console = (function (origConsole) {
 
     return {
         log: function () {
-            this.addLog(arguments);
+            this.addLog(arguments, "logs");
             isDebug && origConsole.log && origConsole.log.apply(origConsole, arguments);
         },
         warn: function () {
-            this.addLog(arguments);
+            this.addLog(arguments, "errors");
             isDebug && origConsole.warn && origConsole.warn.apply(origConsole, arguments);
         },
         error: function () {
-            this.addLog(arguments);
+            this.addLog(arguments, "warns");
             isDebug && origConsole.error && origConsole.error.apply(origConsole, arguments);
         },
         info: function (v) {
-            this.addLog(arguments);
+            this.addLog(arguments, "infos");
             isDebug && origConsole.info && origConsole.info.apply(origConsole, arguments);
         },
         debug: function (bool) {
@@ -35,11 +35,11 @@ window.console = (function (origConsole) {
         saveLog: function (bool) {
             isSaveLog = bool;
         },
-        addLog: function (arguments) {
+        addLog: function (arguments, array) {
             if (!isSaveLog) {
                 return;
             }
-            logArray.logs.push(arguments);
+            logArray[array || "logs"].push(arguments);
         },
         logArray: function () {
             return logArray;
